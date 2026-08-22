ace615a86a8fac0defe797e28dad38171b7f6ca4
shiva <shivaramakrishna082@gmail.com>
2026-08-22 11:15:30 +0530

    intialized project

---
+++ distributedjobprocessingplatform/src/test/java/com/projectone/distributedjobprocessingplatform/DistributedjobprocessingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectone.distributedjobprocessingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedjobprocessingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
